0a739a02f0f99aae883e8c8eb8d644c8a2cff66c
Yashasvi <yashasvi.kumar.sinha@gmail.com>
2017-05-18 15:56:11 +0530

    Added initial files
    Version 1.0

---
+++ script/script.js
@@ -0,0 +1,24 @@
+$("ul").on("click", "li", function (e) {
+  $(this).toggleClass("completed");
+  e.stopPropagation();
+});
+
+// delete 
+$("ul").on("click", "span", function (e) { 
+    $(this).parent().fadeOut(500, function(){
+         $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+//adding todo
+$("input[type = 'text']").keypress(function (e) { 
+    if(e.which === 13){
+        $("ul").append("<li><span><i class='fa fa-window-close' aria-hidden='true'></i></span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
+});
+
+$(".fa-plus-square").click(function () { 
+    $("input[type = 'text']").fadeToggle("0.3");
+});
